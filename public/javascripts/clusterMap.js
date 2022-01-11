fea5f962cbd1f1d92d216677113aa55bcc1e9d07
Jakub Widurek <jakubwidurek@gmail.com>
2022-01-11 15:54:34 +0100

    add cluster map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v10',
-  center: [-103.59179687498357, 40.66995747013945],
-  zoom: 3,
+  center: [-20, 40],
+  zoom: 1,
 });
 
 map.on('load', function () {
