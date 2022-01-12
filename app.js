b98b67d3e255eece0bbb04ddd791debdcf4a2495
Jakub Widurek <jakubwidurek@gmail.com>
2022-01-12 07:50:39 +0100

    use mongo to session store

--- app.js
+++ app.js
@@ -19,9 +19,13 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');  
+const mongoSanitize = require('express-mongo-sanitize');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const MongoDBStore = require("connect-mongo")(session)
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -47,9 +51,22 @@ app.use(mongoSanitize({
   replaceWith: '_'
 }))
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
 const sessionConfig = {
-  secret: 'thisshouldbeabettersecret!',
+  store,
   name: 'session',
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
