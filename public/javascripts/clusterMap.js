ac865fd01eef2cc0766aba1326ac324d4d222c78
Jakub Widurek <jakubwidurek@gmail.com>
2022-01-12 07:30:11 +0100

    add security content

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,10 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: 'map',
+  container: 'cluster-map',
   style: 'mapbox://styles/mapbox/light-v10',
   center: [-20, 40],
   zoom: 1,
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 map.on('load', function () {
   // Add a new source from our GeoJSON data and
